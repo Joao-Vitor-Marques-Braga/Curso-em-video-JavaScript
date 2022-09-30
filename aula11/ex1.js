2e63335ccdad465a357e9a10ec4bf2d7165a22d9
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2022-09-29 21:07:43 -0300

    Add files via upload

---
+++ aula11/ex1.js
@@ -0,0 +1,11 @@
+var velocidade = 90
+
+//condição simples
+if (velocidade < 50){
+    console.log('Velocidade dentro dos limites da via. Dirija com cuidado!')
+}
+
+//condição composta
+else{
+    console.log('Velocidade acima dos limites da via, você acaba de ser multado!')
+}
---
+++ aula11/ex2.js
@@ -0,0 +1,9 @@
+var país = 'França'
+
+if (país == 'Brasil'){
+    console.log('Você é Brasileiro')
+}
+
+else{
+    console.log('Você é estrangeiro')
+}
