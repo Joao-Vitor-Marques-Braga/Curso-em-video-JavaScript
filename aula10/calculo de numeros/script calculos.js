98a78605e055efd6677a7688a70a4105332cc7eb
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2022-10-02 11:45:34 -0300

    Delete aula10 directory

--- aula10/calculo de numeros/script calculos.js
+++
@@ -1,9 +0,0 @@
-function somar(){
-    var ns1 = window.document.getElementById('number1')
-    var ns2 = window.document.getElementById('number2')
-    var resultado = window.document.getElementById('res')
-    var n1 = Number(ns1.value)
-    var n2 = Number(ns2.value)
-    var soma = n1+n2
-    resultado.innerText = `A soma entre os números informados é: ${soma}`
-}
